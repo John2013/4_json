8f3b083a7a60d50accff7001cd51a333c13572f4
John2013 <NaWasington@yandex.ru>
2017-12-31 16:29:44 +0300

    tabs → spaces

--- pprint_json.py
+++ pprint_json.py
@@ -5,13 +5,13 @@ from pprint import pprint
 
 
 def pprint_from_file(filepath):
-	with open(filepath, encoding='utf-8') as f:
-		pprint(json.load(f))
+    with open(filepath, encoding='utf-8') as f:
+        pprint(json.load(f))
 
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		file_path = sys.argv[1]
-		pprint_from_file(file_path)
-	else:
-		assert AttributeError('Отсутсвует путь к файлу')
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        pprint_from_file(file_path)
+    else:
+        assert AttributeError('Отсутсвует путь к файлу')
