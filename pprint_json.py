cd18cfaceadbe3bda89d04dd5a4bc9c25c2ee241
John2013 <NaWasington@yandex.ru>
2018-01-02 13:35:22 +0300

    changed functions

--- pprint_json.py
+++ pprint_json.py
@@ -1,17 +1,20 @@
 import json
 
 import sys
-from pprint import pprint
 
 
-def pprint_from_file(filepath):
-    with open(filepath, encoding='utf-8') as f:
-        pprint(json.load(f))
+def load_json_from_file(file_path):
+    with open(file_path, encoding='utf-8') as file:
+        return json.load(file)
+
+
+def json_pprint(json_data):
+    print(json.dumps(json_data, indent=4, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
-        pprint_from_file(file_path)
+        json_pprint(load_json_from_file(file_path))
     else:
         assert AttributeError('Отсутсвует путь к файлу')
