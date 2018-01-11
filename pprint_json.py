4df01f7a04ed54296ea6ad10fbf6de6ccb677b89
John2013 <NaWasington@yandex.ru>
2018-01-11 17:38:17 +0300

    Добавил проверку существования файла

--- pprint_json.py
+++ pprint_json.py
@@ -2,6 +2,8 @@ import json
 
 import sys
 
+from os.path import isfile
+
 
 def load_json_from_file(file_path):
     with open(file_path, encoding='utf-8') as file:
@@ -15,6 +17,11 @@ def json_pprint(json_data):
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_path = sys.argv[1]
-        json_pprint(load_json_from_file(file_path))
     else:
-        assert AttributeError('Отсутсвует путь к файлу')
+        file_path = ""
+
+    while not isfile(file_path):
+        print("Неверный путь к файлу")
+        file_path = input("Введите путь к файлу: ")
+
+    json_pprint(load_json_from_file(file_path))
